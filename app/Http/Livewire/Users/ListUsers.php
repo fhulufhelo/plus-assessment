60878d390c8089570d58ea547e6cf4ef452d7559
Fhulufhelo Mokhomi <mokhomifhulufhelo8@gmail.com>
2023-03-15 23:35:18 +0200

    add all

--- app/Http/Livewire/Users/ListUsers.php
+++ app/Http/Livewire/Users/ListUsers.php
@@ -4,14 +4,23 @@ namespace App\Http\Livewire\Users;
 
 use App\Models\User;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class ListUsers extends Component
 {
+    use WithPagination;
+
+    public $search;
+
+    protected $queryString = ['search'];
+
     protected $listeners = ['userAdded' => '$refresh'];
 
-    public function getUsersProperty(): \Illuminate\Database\Eloquent\Collection|array
+    public function getUsersProperty(): \Illuminate\Contracts\Pagination\LengthAwarePaginator
     {
-        return User::with('roles')->get();
+        return User::with('roles')
+            ->where('first_name', 'like', '%'.$this->search.'%')
+            ->paginate(2);
     }
 
     public function edit($id)
